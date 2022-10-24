44135896ba06baad9cfd8fde40bc5c663e9621a8
Masum Mollo <freelancermasummolla@gmail.com>
2022-10-24 20:33:01 +0600

    init

---
+++ posts-to-qrcode.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Posts To QR Code
+Plugin URL: http://masummollaalhaz.com/plugins/
+Description: Display QR code under every posts
+Version: 1.0.0
+Author: Md Masum Molla Alhaz
+Author URL: http://masummollaalhaz.com/
+License: GPLv2 or later
+Text Domain: posts-to-qrcode
+Domain Path: /languages/
+*/
+
+
+/*
+function qrcode_activation_hook(){
+    register_activation_hook(__FILE__, "qrcode_activation_hook")
+}
+
+function qrcode_deactivation_hook(){
+    register_deactivation_hook(__FILE__, "qrcode_deactivation_hook")
+}
+*/
+
+
+
+
+
+
+?>
