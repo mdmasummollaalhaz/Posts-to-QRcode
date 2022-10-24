ef8020fcd28b3fc0f88cfaaae32b59f68344bfa1
Masum Mollo <freelancermasummolla@gmail.com>
2022-10-24 21:02:57 +0600

    display qr code

--- posts-to-qrcode.php
+++ posts-to-qrcode.php
@@ -23,7 +23,18 @@ function qrcode_deactivation_hook(){
 */
 
 
-
+function qrcode_load_textdomain(){
+    load_plugin_textdomain( 'posts_to_qrcode', false, dirname(__FILE__) ."/languages/");
+}
+function pqrc_display_qr_code($content){
+    $current_post_id = get_the_ID();
+    $current_post_title = get_the_title( $current_post_id );
+    $current_post_url = urlencode(get_the_permalink($current_post_id));
+    $image_src = sprintf('https://api.qrserver.com/v1/create-qr-code/?size=185x185&ecc=L&qzone=1&data=%s', $current_post_url);
+    $content .= sprintf("<div class='qrcode'><img src='%s' alt='%s' ></div>", $image_src, $current_post_title);
+    return $content;
+}
+add_filter('the_content', 'pqrc_display_qr_code')
 
 
 
